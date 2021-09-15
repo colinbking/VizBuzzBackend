dd5ec6e08401cdbd14d2416487207f4fa906b5f1
michael.sprintson@cognite.com <51164633+michaelsprintson@users.noreply.github.com>
2021-09-14 22:36:25 -0500

    new nlp simple function, test, and update to requirements

---
+++ BackendConfig/API/nlp.py
@@ -0,0 +1,9 @@
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+
+
+def what_color_is_this_sentence(text):
+    nlp = spacy.load('en_core_web_sm')
+    nlp.add_pipe("spacytextblob")
+    doc = nlp(text) 
+    return "green" if doc._.polarity > 0 else "red"
