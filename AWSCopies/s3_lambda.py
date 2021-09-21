f096c48cc3b73fd4742e8c1e5c2eacb64922fc31
Prashanth Pai <prashanthpai55@gmail.com>
2021-09-20 22:44:00 -0500

    Fixed flake8

--- AWSCopies/s3_lambda.py
+++ AWSCopies/s3_lambda.py
@@ -1,4 +1,3 @@
-import json
 import urllib.parse
 import boto3
 
@@ -7,7 +6,7 @@ s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
+    # print("Received event: " + json.dumps(event, indent=2))
 
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
@@ -18,5 +17,6 @@ def lambda_handler(event, context):
         return response['ContentType']
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        print('Error getting object {} from bucket {}.' +
+              'Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
