a7e293404a8a6ac3b1f86e0404b39c3822228f13
Colin King <cbk1@rice.edu>
2021-09-27 15:53:30 -0500

    changing transcriber location to be in API

--- BackendConfig/API/views.py
+++ BackendConfig/API/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from .models import Transcript
 from .serializers import TranscriptSerializer
-from ..Transcriber.transcriber import *
+from .Transcriber.transcriber import *
 import json
 
 
