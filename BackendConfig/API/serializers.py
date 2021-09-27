93024a65ee18e105c1ef5d0702b869b57f562b02
Colin King <cbk1@rice.edu>
2021-09-27 15:02:28 -0500

    adding models and serializers and views

--- BackendConfig/API/serializers.py
+++ BackendConfig/API/serializers.py
@@ -3,10 +3,24 @@
 # the appropriate model for backend manipulation.
 
 from rest_framework import serializers
-from .models import Transcript
+from .models import *
 
 
 class TranscriptSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Transcript
         fields = ('name', 'alias', 'color', 'all_text')
+
+
+class PodcastSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Podcast
+        fields = ('id', 's3_audio_id', 's3_transcript_id', 'name', 'episode_number', 'author', 'publsih_date', 'rss_url')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'username', 'favorites', 'password', 'google_login_info')
+
+
