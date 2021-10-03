d204d73918fecc00c72a4588a9ae528b300d066e
michael.sprintson@cognite.com <51164633+michaelsprintson@users.noreply.github.com>
2021-10-03 16:49:46 -0500

    new output format system - back to file type audio

--- BackendConfig/API/Transcriber/transcriber.py
+++ BackendConfig/API/Transcriber/transcriber.py
@@ -4,14 +4,18 @@ from dotenv import load_dotenv
 import time
 import json
 from functools import reduce
-import wave
-import audioop
+# import boto3
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+
+load_dotenv()
 
 compresslambda = lambda x: [j for i in x for j in i]
 addlambda = lambda x: reduce(lambda a,b: a + " " + b, x)
 
-def add_key(d, k, v):
+def add_key(d, k, v, i):
     d[k] = v
+    d['index'] = i
     return d
 
 class Transcriber():
@@ -19,9 +23,13 @@ class Transcriber():
     # our transcript s3 bucket.
     def transcribe(self, bucket, key):
         print("transcribing audio file with key: ", key)
+        # s3 = boto3.client('s3')
+        # response = s3.get_object(Bucket=bucket, Key=key)
+        # print("CONTENT TYPE: " + response['ContentType'])
         vzsr = vz_speech_recog()
-        sr = vzsr.speech_recognize_continuous_from_file("The_smoking_tire_daniel_osborne.wav")
+        vzsr.speech_recognize_continuous_from_file("The_smoking_tire_daniel_osborne.wav");
         output_format = vzsr.create_output()
+        print(output_format)
         ## what do i do now with this output format?
         return True
 
@@ -38,9 +46,7 @@ class vz_speech_recog:
         speech_config.request_word_level_timestamps()
 
         # <SpeechContinuousRecognitionWithFile>
-        stream = speechsdk.audio.PushAudioInputStream()
-        audio_config = speechsdk.audio.AudioConfig(stream=stream)
-        # audio_config = speechsdk.audio.AudioConfig(filename=filename)
+        audio_config = speechsdk.audio.AudioConfig(filename=filename)
 
         speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
@@ -52,46 +58,23 @@ class vz_speech_recog:
             nonlocal done
             done = True
 
+        # Connect callbacks to the events fired by the speech recognizer
+    #     speech_recognizer.recognizing.connect(lambda evt: test_print(evt))
         speech_recognizer.recognized.connect(lambda evt: self.save_transcript(evt))
+#         speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+#         speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+#         speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+#         stop continuous recognition on either session stopped or canceled events
         speech_recognizer.session_stopped.connect(stop_cb)
         speech_recognizer.canceled.connect(stop_cb)
 
-        n_bytes = 3200
-        wav_fh = wave.open(filename) # filename can be a file-like object, which is what we will retrieve from AWS
-        
-        print(wav_fh.getnchannels(), 1)
-        print(wav_fh.getsampwidth(), 2)
-        print(wav_fh.getframerate(), 16000)
-        print(wav_fh.getcomptype(), "NONE")
-        
-        if(wav_fh.getnchannels() != 1) or (wav_fh.getframerate() != 16000):
-            res, loc = downsampleWav(filename, filename[:-4] + "resampled.wav", inrate=wav_fh.getframerate(), outrate=16000, inchannels=wav_fh.getnchannels(), outchannels=1)
-            if res:
-                wav_fh.close()
-                wav_fh = wave.open(loc)
-            else:
-                print("something broke!!")
-        
-        print(wav_fh.getnchannels(), 1)
-        print(wav_fh.getsampwidth(), 2)
-        print(wav_fh.getframerate(), 16000)
-        print(wav_fh.getcomptype(), "NONE")
         # Start continuous speech recognition
-        # speech_recognizer.start_continuous_recognition()
-        # try:
-        #     while(True):
-        #         frames = wav_fh.readframes(n_bytes // 2)
-        #         # print('read {} bytes'.format(len(frames)))
-        #         if not frames:
-        #             break
+        speech_recognizer.start_continuous_recognition()
+        while not done:
+            time.sleep(.5)
 
-        #         stream.write(frames)
-        #         time.sleep(.1)
-        # finally:
-        #     # stop recognition and clean up
-        #     wav_fh.close()
-        #     stream.close()
-        #     speech_recognizer.stop_continuous_recognition()
+        speech_recognizer.stop_continuous_recognition()
+        return speech_recognizer
         # </SpeechContinuousRecognitionWithFile>
 
     def save_transcript(self, istr):
@@ -123,45 +106,34 @@ class vz_speech_recog:
         # assert(len(lex_words) == len(words_and_offsets))
         words_and_offsets = compresslambda([self.best_lexs[i]['Words'] for i in range(len(self.best_lexs))])
         lex_words = addlambda([self.best_lexs[i]['Lexical'] for i in range(len(self.best_lexs))]).split(" ")
-        return [add_key(d, "display", l) for d, l in zip(words_and_offsets, lex_words)]
+        self.lex_final = lex_words
+        z = list(zip(words_and_offsets, lex_words))
+        mid_output = [add_key(z[zi][0], "display", z[zi][1], zi) for zi in range(len(z))]
 
+        final_chuncks = [{"Words": [], "start_index": 0}]
+        curr_index = 0
+        seen_words = set()
+        for i, w in enumerate(self.lex_final[:240]):
+            if w in seen_words:
+                curr_index += 1
+                final_chuncks.append({"Words": [w], "start_index": i})
+                seen_words = set()
+            else:
+                seen_words.add(w)
+                final_chuncks[curr_index]['Words'].append(w)
 
-def downsampleWav(src, dst, inrate=44100, outrate=16000, inchannels=2, outchannels=1):
-    print("-------------", inrate, outrate, inchannels, outchannels)
-    if not os.path.exists(src):
-        print ('Source not found!')
-        return False, ""
+        for lineiq in final_chuncks:
+            sentiq = reduce(lambda a,b: a + " " + b, lineiq['Words'])
+            nlp = spacy.load('en_core_web_sm')
+            nlp.add_pipe("spacytextblob")
+            doc = nlp(sentiq)
+            print(sentiq, doc._.polarity, doc._.subjectivity)
+            for phraseassign in doc._.assessments:
+                # print(phraseassign)
+                for wordassign in phraseassign[0]:
+                    idx = lineiq['Words'].index(wordassign) + lineiq['start_index']
+                    print(wordassign, phraseassign[1], idx)
+                    mid_output[idx]['Polarity'] = phraseassign[1]
+                    mid_output[idx]['Subjective'] = phraseassign[2]
 
-    print(dst)
-    dst_dir = os.path.dirname(__file__) + "/resampled_files/"
-    print(dst_dir)
-
-
-    try:
-        s_read = wave.open(src, 'r')
-        s_write = wave.open(dst_dir + dst, 'w')
-    except:
-        print ('Failed to open files!')
-        return False, ""
-
-    n_frames = s_read.getnframes()
-    data = s_read.readframes(n_frames)
-
-    try:
-        converted = audioop.ratecv(data, 2, inchannels, inrate, outrate, None)
-        if outchannels == 1:
-            if inchannels != 1:
-                converted = audioop.tomono(converted[0], 2, 1, 0)
-    except:
-        print ('Failed to downsample wav')
-        return False, ""
-
-    try:
-        s_write.setparams((outchannels, 2, outrate, 0, 'NONE', 'Uncompressed'))
-        print('got to setparams')
-        s_write.writeframes(converted)
-        return True, dst_dir + dst
-    except Exception as e:
-        print(e)
-        print ('Failed to write wav')
-        return False, ""
+        return mid_output
