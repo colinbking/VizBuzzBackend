6b1b4ea30740307863980bc37527d5c1703767d3
Prashanth Pai <prashanthpai55@gmail.com>
2021-10-08 15:16:51 -0500

    Fixed bucket/key code

--- BackendConfig/API/Transcriber/transcriber.py
+++ BackendConfig/API/Transcriber/transcriber.py
@@ -21,9 +21,6 @@ class Transcriber():
     def download_file(self, bucket, key):
         print("downloading file")
         s3 = boto3.client('s3')
-
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
         try:
             response = s3.get_object(Bucket=bucket, Key=key)
             print("CONTENT TYPE: " + response['ContentType'])
