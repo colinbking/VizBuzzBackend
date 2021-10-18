6e27f458bc7cd564f9bb015a02d6772b6a8d6c36
Prashanth Pai <prashanthpai55@gmail.com>
2021-10-18 18:58:53 -0500

    Fixed transcript get

--- BackendConfig/API/views.py
+++ BackendConfig/API/views.py
@@ -26,7 +26,7 @@ class TranscriptView(views.APIView):
             transcript_bucket_id=json_data['transcript_bucket_id']
             transcript_file_id=json_data['transcript_file_id']
             s3 = boto3.client('s3')
-            return JsonResponse(json.loads(s3.get_object(Bucket=transcript_bucket_id, Key=transcript_file_id)['Body'].read()), status=200)
+            return JsonResponse(json.loads(s3.get_object(Bucket=transcript_bucket_id, Key=transcript_file_id)['Body'].read()), safe=False, status=200)
 
         except KeyError:
             return Response("transcript_bucket_id or transcript_file_id not found in request body", status=400)
