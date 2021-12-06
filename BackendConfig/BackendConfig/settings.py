6553a563e02f685d23b137d54a82e8654955e914
colinbking <46637699+colinbking@users.noreply.github.com>
2021-12-05 23:09:24 -0600

    Update settings.py

--- BackendConfig/BackendConfig/settings.py
+++ BackendConfig/BackendConfig/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'vizbuzz-backend.herokuapp.com',
     'vizbuzz-backend-dev.herokuapp.com',
+    'https://vizbuzz.azurewebsites.net',
     'localhost',
     '127.0.0.1'
 ]
