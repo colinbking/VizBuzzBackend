abca6616b49e34b8e3b84446e550278fcdac85ae
Colin King <cbk1@rice.edu>
2021-12-08 18:22:43 -0600

    adding print debug

--- BackendConfig/API/views/auth_views.py
+++ BackendConfig/API/views/auth_views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponseForbidden
 from rest_framework import views
 from django.http import HttpResponse
 from rest_framework.response import Response
@@ -15,44 +16,47 @@ class LoginView(views.APIView):
     Stores user id in session.
     """
     def post(self, request):
-        username = request.data['username']
-        password = request.data['password']
+        try:
+            username = request.data['username']
+            password = request.data['password']
 
-        user = User.objects.filter(username=username).first()
+            user = User.objects.filter(username=username).first()
 
-        if user is None:
-            raise AuthenticationFailed('User not found!')
+            if user is None:
+                raise AuthenticationFailed('User not found!')
 
-        if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect password!')
+            if not user.check_password(password):
+                raise AuthenticationFailed('Incorrect password!')
 
-        access_payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
-            'iat': datetime.datetime.utcnow()
-        }
+            access_payload = {
+                'id': user.id,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+                'iat': datetime.datetime.utcnow()
+            }
 
-        refresh_payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=360),
-            'iat': datetime.datetime.utcnow()
-        }
+            refresh_payload = {
+                'id': user.id,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=360),
+                'iat': datetime.datetime.utcnow()
+            }
 
-        access_token = jwt.encode(access_payload, 'secret', algorithm='HS256')
-        refresh_token = jwt.encode(refresh_payload, 'secret', algorithm='HS256')
-        response = Response()
+            access_token = jwt.encode(access_payload, 'secret', algorithm='HS256')
+            refresh_token = jwt.encode(refresh_payload, 'secret', algorithm='HS256')
+            response = Response()
 
-        response.set_cookie(key='access', value=access_token, httponly=True)
-        response.set_cookie(key='refresh', value=refresh_token, httponly=True)
+            response.set_cookie(key='access', value=access_token, httponly=True)
+            response.set_cookie(key='refresh', value=refresh_token, httponly=True)
 
-        response.data = {
-            'access': access_token,
-            'refresh': refresh_token
-        }
+            response.data = {
+                'access': access_token,
+                'refresh': refresh_token
+            }
 
-        request.session['user_id'] = user.id
-
-        return response
+            request.session['user_id'] = user.id
+        
+            return response
+        except Exception as e:
+            return HttpResponse("error:" + str(e))
 
 
 class RefreshView(views.APIView):
